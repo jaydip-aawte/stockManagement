58caab7c12bf44af789e3bd78a6e92cf5c75494d
jaydip-cloudesign <jaydip.aawte@cloudesign.com>
2023-10-05 00:01:00 +0530

    initial commit

---
+++ database/ration.sql
@@ -0,0 +1,480 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2023 at 04:09 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `back_order_list`
+--
+
+CREATE TABLE `back_order_list` (
+  `id` int(30) NOT NULL,
+  `receiving_id` int(30) NOT NULL,
+  `po_id` int(30) NOT NULL,
+  `bo_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `discount_perc` float NOT NULL DEFAULT '0',
+  `discount` float NOT NULL DEFAULT '0',
+  `tax_perc` float NOT NULL DEFAULT '0',
+  `tax` float NOT NULL DEFAULT '0',
+  `remarks` text,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 = pending, 1 = partially received, 2 =received',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `back_order_list`
+--
+
+INSERT INTO `back_order_list` (`id`, `receiving_id`, `po_id`, `bo_code`, `supplier_id`, `amount`, `discount_perc`, `discount`, `tax_perc`, `tax`, `remarks`, `status`, `date_created`, `date_updated`) VALUES
+(1, 1, 1, 'BO-0001', 1, 40740, 3, 1125, 12, 4365, NULL, 1, '2021-11-03 11:20:38', '2021-11-03 11:20:51'),
+(2, 2, 1, 'BO-0002', 1, 20370, 3, 562.5, 12, 2182.5, NULL, 2, '2021-11-03 11:20:51', '2021-11-03 11:21:00'),
+(3, 4, 2, 'BO-0003', 2, 42826, 5, 2012.5, 12, 4588.5, NULL, 1, '2021-11-03 11:51:41', '2021-11-03 11:52:02'),
+(4, 5, 2, 'BO-0004', 2, 10640, 5, 500, 12, 1140, NULL, 2, '2021-11-03 11:52:02', '2021-11-03 11:52:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bo_items`
+--
+
+CREATE TABLE `bo_items` (
+  `bo_id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `price` float NOT NULL DEFAULT '0',
+  `unit` varchar(50) NOT NULL,
+  `total` float NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bo_items`
+--
+
+INSERT INTO `bo_items` (`bo_id`, `item_id`, `quantity`, `price`, `unit`, `total`) VALUES
+(1, 1, 250, 150, 'kilo', 37500),
+(2, 1, 125, 150, 'kilo', 18750),
+(3, 2, 150, 200, 'kilo', 30000),
+(3, 4, 50, 205, 'kilo', 10250),
+(4, 2, 50, 200, 'kilo', 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_list`
+--
+
+CREATE TABLE `item_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `cost` float NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `item_list`
+--
+
+INSERT INTO `item_list` (`id`, `name`, `description`, `supplier_id`, `cost`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'Wheat', 'Sample Only', 1, 5, 1, '2021-11-02 10:01:55', '2023-02-02 20:18:55'),
+(2, 'Sugar', 'Sample only', 2, 10, 1, '2021-11-02 10:02:12', '2023-02-02 20:19:05'),
+(3, 'Oil', 'Sample', 1, 140, 1, '2021-11-02 10:02:27', '2023-02-02 20:19:23'),
+(4, 'Rice', 'Sample only', 2, 4, 1, '2021-11-02 10:02:47', '2023-02-02 20:18:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `po_items`
+--
+
+CREATE TABLE `po_items` (
+  `po_id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `price` float NOT NULL DEFAULT '0',
+  `unit` varchar(50) NOT NULL,
+  `total` float NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `po_items`
+--
+
+INSERT INTO `po_items` (`po_id`, `item_id`, `quantity`, `price`, `unit`, `total`) VALUES
+(2, 2, 300, 200, 'Boxes', 60000),
+(2, 4, 200, 205, 'pcs', 41000),
+(1, 1, 1000, 5, 'kilo', 5000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_order_list`
+--
+
+CREATE TABLE `purchase_order_list` (
+  `id` int(30) NOT NULL,
+  `po_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL,
+  `discount_perc` float NOT NULL DEFAULT '0',
+  `discount` float NOT NULL DEFAULT '0',
+  `tax_perc` float NOT NULL DEFAULT '0',
+  `tax` float NOT NULL DEFAULT '0',
+  `remarks` text NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0 = pending, 1 = partially received, 2 =received',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `purchase_order_list`
+--
+
+INSERT INTO `purchase_order_list` (`id`, `po_code`, `supplier_id`, `amount`, `discount_perc`, `discount`, `tax_perc`, `tax`, `remarks`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'PO-0001', 1, 5432, 3, 150, 12, 582, 'Ration', 2, '2021-11-03 11:20:22', '2023-02-02 20:26:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receiving_list`
+--
+
+CREATE TABLE `receiving_list` (
+  `id` int(30) NOT NULL,
+  `form_id` int(30) NOT NULL,
+  `from_order` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=PO ,2 = BO',
+  `amount` float NOT NULL DEFAULT '0',
+  `discount_perc` float NOT NULL DEFAULT '0',
+  `discount` float NOT NULL DEFAULT '0',
+  `tax_perc` float NOT NULL DEFAULT '0',
+  `tax` float NOT NULL DEFAULT '0',
+  `stock_ids` text,
+  `remarks` text,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receiving_list`
+--
+
+INSERT INTO `receiving_list` (`id`, `form_id`, `from_order`, `amount`, `discount_perc`, `discount`, `tax_perc`, `tax`, `stock_ids`, `remarks`, `date_created`, `date_updated`) VALUES
+(1, 1, 1, 40740, 3, 1125, 12, 4365, '1', 'Sample', '2021-11-03 11:20:38', '2021-11-03 11:20:38'),
+(2, 1, 2, 20370, 3, 562.5, 12, 2182.5, '2', '', '2021-11-03 11:20:51', '2021-11-03 11:20:51'),
+(3, 2, 2, 20370, 3, 562.5, 12, 2182.5, '3', 'Success', '2021-11-03 11:21:00', '2021-11-03 11:21:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_list`
+--
+
+CREATE TABLE `return_list` (
+  `id` int(30) NOT NULL,
+  `return_code` varchar(50) NOT NULL,
+  `supplier_id` int(30) NOT NULL,
+  `amount` float NOT NULL DEFAULT '0',
+  `remarks` text,
+  `stock_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_list`
+--
+
+CREATE TABLE `sales_list` (
+  `id` int(30) NOT NULL,
+  `sales_code` varchar(50) NOT NULL,
+  `client` text,
+  `amount` float NOT NULL DEFAULT '0',
+  `remarks` text,
+  `stock_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales_list`
+--
+
+INSERT INTO `sales_list` (`id`, `sales_code`, `client`, `amount`, `remarks`, `stock_ids`, `date_created`, `date_updated`) VALUES
+(1, 'SALE-0001', 'Ganesh Adhikari', 228, 'Customer Sales on date', '0,0,0', '2021-11-03 14:03:30', '2023-02-02 20:23:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock_list`
+--
+
+CREATE TABLE `stock_list` (
+  `id` int(30) NOT NULL,
+  `item_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `unit` varchar(250) DEFAULT NULL,
+  `price` float NOT NULL DEFAULT '0',
+  `total` float NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock_list`
+--
+
+INSERT INTO `stock_list` (`id`, `item_id`, `quantity`, `unit`, `price`, `total`, `type`, `date_created`) VALUES
+(1, 1, 250, 'pcs', 150, 37500, 1, '2021-11-03 11:20:38'),
+(2, 1, 125, 'pcs', 150, 18750, 1, '2021-11-03 11:20:51'),
+(3, 1, 125, 'pcs', 150, 18750, 1, '2021-11-03 11:21:00'),
+(0, 3, 1, 'liter', 140, 140, 2, '2023-02-02 20:23:58'),
+(0, 4, 12, 'kilo', 4, 48, 2, '2023-02-02 20:23:58'),
+(0, 1, 8, 'kilo', 5, 40, 2, '2023-02-02 20:23:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_list`
+--
+
+CREATE TABLE `supplier_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `cperson` text NOT NULL,
+  `contact` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supplier_list`
+--
+
+INSERT INTO `supplier_list` (`id`, `name`, `address`, `cperson`, `contact`, `status`, `date_created`, `date_updated`) VALUES
+(1, 'Ashok Joshi', 'Panvel', 'ashok Joshi', '09123456789', 1, '2021-11-02 09:36:19', '2023-02-02 20:02:31'),
+(2, 'Sunil Gopale', 'thane', 'Sunil Gopale', '0987654332', 1, '2021-11-02 09:36:54', '2023-02-02 20:02:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Ration-Stock Management System'),
+(6, 'short_name', 'RationManagement'),
+(11, 'logo', 'uploads/logo-1635816671.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/cover-1635816671.png'),
+(15, 'content', 'Array');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `middlename` text,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT '0',
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1635556826', NULL, 1, '2021-01-20 14:02:37', '2021-10-30 09:20:26'),
+(11, 'admin', NULL, 'admin', 'admin2', 'cd74fae0a3adf459f73bbf187607ccea', 'uploads/avatar-11.png?v=1675348726', NULL, 1, '2021-11-03 14:22:46', '2023-02-02 20:08:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meta`
+--
+
+CREATE TABLE `user_meta` (
+  `user_id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `back_order_list`
+--
+ALTER TABLE `back_order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`),
+  ADD KEY `po_id` (`po_id`),
+  ADD KEY `receiving_id` (`receiving_id`);
+
+--
+-- Indexes for table `bo_items`
+--
+ALTER TABLE `bo_items`
+  ADD KEY `item_id` (`item_id`),
+  ADD KEY `bo_id` (`bo_id`);
+
+--
+-- Indexes for table `item_list`
+--
+ALTER TABLE `item_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `po_items`
+--
+ALTER TABLE `po_items`
+  ADD KEY `po_id` (`po_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `purchase_order_list`
+--
+ALTER TABLE `purchase_order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `receiving_list`
+--
+ALTER TABLE `receiving_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `return_list`
+--
+ALTER TABLE `return_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `sales_list`
+--
+ALTER TABLE `sales_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier_list`
+--
+ALTER TABLE `supplier_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_meta`
+--
+ALTER TABLE `user_meta`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `back_order_list`
+--
+ALTER TABLE `back_order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `item_list`
+--
+ALTER TABLE `item_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `purchase_order_list`
+--
+ALTER TABLE `purchase_order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `receiving_list`
+--
+ALTER TABLE `receiving_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `return_list`
+--
+ALTER TABLE `return_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sales_list`
+--
+ALTER TABLE `sales_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
